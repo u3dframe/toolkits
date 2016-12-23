212ae6b4c44770229772c12c9b0dd6532d82c384
Canyon <379657505@qq.com>
2016-12-23 15:41:36 +0800

    添加字符串常用的方法

--- Assets/_Frame/Scripts/Toolkits/StrEx.cs
+++ Assets/_Frame/Scripts/Toolkits/StrEx.cs
@@ -27,5 +27,86 @@ namespace Toolkits
 			src = src.Replace (".", "_");
 			return src;
 		}
-	}
+
+        static public bool isBefore(string dateStr1, string dateStr2)
+        {
+            if (string.IsNullOrEmpty(dateStr1) || string.IsNullOrEmpty(dateStr2))
+                return false;
+            int v = dateStr1.CompareTo(dateStr2);
+            bool flag = v >= -1;
+            return flag;
+        }
+
+        static public bool isAfter(string dateStr1, string dateStr2)
+        {
+            if (string.IsNullOrEmpty(dateStr1) || string.IsNullOrEmpty(dateStr2))
+                return false;
+            int v = dateStr1.CompareTo(dateStr2);
+            bool flag = v >= 1;
+            return flag;
+        }
+
+        static public bool isSame(string dateStr1, string dateStr2)
+        {
+            if (string.IsNullOrEmpty(dateStr1) || string.IsNullOrEmpty(dateStr2))
+                return false;
+            int v = dateStr1.CompareTo(dateStr2);
+            bool flag = v == 0;
+            return flag;
+        }
+
+        static public bool isNotAfter(string dateStr1, string dateStr2)
+        {
+            if (string.IsNullOrEmpty(dateStr1) || string.IsNullOrEmpty(dateStr2))
+                return false;
+            int v = dateStr1.CompareTo(dateStr2);
+            bool flag = v <= 0;
+            return flag;
+        }
+
+
+        static public bool isNotBefore(string dateStr1, string dateStr2)
+        {
+            if (string.IsNullOrEmpty(dateStr1) || string.IsNullOrEmpty(dateStr2))
+                return false;
+            int v = dateStr1.CompareTo(dateStr2);
+            bool flag = v >= 0;
+            return flag;
+        }
+
+        static public bool isStr(object objPars)
+        {
+            if (objPars == null)
+                return false;
+            return objPars.GetType() == typeof(string);
+        }
+
+        static public int getLens(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return 0;
+            return str.Length;
+        }
+
+        static public int getLens4Trim(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return 0;
+            str = str.Trim();
+            return str.Length;
+        }
+
+        static public string trimStr(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return "";
+            return str.Trim();
+        }
+
+        static public bool isNullEmpty(string str)
+        {
+            int lens = getLens4Trim(str);
+            return lens <= 0;
+        }
+    }
 }
