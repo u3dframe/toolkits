56a088b4dee1729ed481d9d56e6a9f9f3f009770
Canyon <379657505@qq.com>
2016-12-23 15:16:19 +0800

    添加文件，路径，MD5，字符串，客户端唯一标示工具

---
+++ Assets/_Frame/Scripts/Toolkits/StrEx.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+/// <summary>
+/// 类名 : 字符串操作工具
+/// 作者 : Canyon
+/// 日期 : 2016-12-23 15:20:00
+/// 功能 : 
+/// </summary>
+namespace Toolkits
+{
+	public static class StrEx
+	{
+		// 转换路径文件
+		static public string replaceSeparator(string src){
+			if(string.IsNullOrEmpty(src)){
+				return "";
+			}
+			return src.Replace("\\","/");
+		}
+
+		static public string replace2Underline(string src){
+			if(string.IsNullOrEmpty(src)){
+				return "";
+			}
+			src = replaceSeparator (src);
+			src = src.Replace ("/", "_");
+			src = src.Replace (".", "_");
+			return src;
+		}
+	}
+}
