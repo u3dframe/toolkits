836a530a991c49b7c0317d261465e5e53a0ffa7f
Canyon <379657505@qq.com>
2016-12-26 11:12:19 +0800

    抽象出来的粒子进行编写

--- Assets/_Frame/Scripts/Game/UIExParticleSystem.cs
+++ Assets/_Frame/Scripts/Game/UIExParticleSystem.cs
@@ -9,125 +9,39 @@ using Toolkits;
 /// 功能 :  
 /// </summary>
 public class UIExParticleSystem : MonoBehaviour {
-    List<ParticleSystem> listAll = new List<ParticleSystem>();
-    List<Renderer> listAllRenders = new List<Renderer>();
 
-    Dictionary<int, List<float>> dicDefaultScale = new Dictionary<int, List<float>>();
-
-    int lens = 0;
-    float curSize = -1;
-    float _curScale = 1;
-    bool _curState = false;
-    
-    // 该粒子的最长时间
-    float _maxTime = 1f;
+    DBU3D_Particle m_db_particle = new DBU3D_Particle();
 
     void Awake()
     {
-        listAll.Clear();
-        ParticleSystem[] arr = transform.GetComponentsInChildren<ParticleSystem>(true);
-        Renderer[] arrRenders = transform.GetComponentsInChildren<Renderer>(true);
-        if (arrRenders != null && arrRenders.Length > 0)
-        {
-            listAllRenders.AddRange(arrRenders);
-        }
+        m_db_particle.DoReInit(transform);
+    }
 
-        if (arr != null && arr.Length > 0)
-        {
-            listAll.AddRange(arr);
-        }
-
-        lens = listAll.Count;
-        if (lens <= 0)
-        {
-            return;
-        }
-
-        ParticleSystem ps;
-        float curTime = 0f;
-        int key;
-        List<float> vList;
-        for (int i = 0; i < lens; i++)
-        {
-            ps = listAll[i];
-            curTime = ps.startDelay + ps.duration;
-            if (curTime > _maxTime)
-            {
-                _maxTime = curTime;
-            }
-
-            key = ps.GetInstanceID();
-            vList = new List<float>();
-            vList.Add(ps.startSpeed);
-            vList.Add(ps.startSize);
-            vList.Add(ps.gravityModifier);
-            dicDefaultScale.Add(key, vList);
-        }
+    void OnDestroy()
+    {
+        m_db_particle.DoClear();
     }
 
     public void setSize(float size)
     {
-        if (lens <= 0 || size < 0 || size == curSize)
-        {
-            return;
-        }
-        curSize = size;
-        ParticleSystem ps;
-        for (int i = 0; i < lens; i++)
-        {
-            ps = listAll[i];
-            ps.startSize = size;
-        }
+        m_db_particle.SetSize(size);
     }
 
     public void setScale(float _scale)
     {
-        if (lens <= 0 || _scale < 0 || _scale == _curScale)
-        {
-            return;
-        }
-        _curScale = _scale;
-        
-        ParticleSystem ps;
-        List<float> vList;
-        for (int i = 0; i < lens; i++)
-        {
-            ps = listAll[i];
-            ps.Clear();     //例子prewarm 引起的bug, fix by haoc
-            vList = dicDefaultScale[ps.GetInstanceID()];
-            ps.startSpeed = _scale * (vList[0]);
-            ps.startSize = _scale * (vList[1]);
-            ps.gravityModifier = _scale * (vList[2]);
-        }
+        m_db_particle.SetScale(_scale);
     }
 
     public void changeState(bool _isPause)
     {
-        if (lens <= 0 || _curState == _isPause)
-        {
-            return;
-        }
-        ParticleSystem ps;
-        _curState = _isPause;
-        for (int i = 0; i < lens; i++)
-        {
-            ps = listAll[i];
-            if (_curState)
-            {
-                ps.Pause();
-            }
-            else
-            {
-                ps.Play();
-            }
-        }
+        m_db_particle.ChangeState(_isPause);
     }
 
     public float curScale
     {
         get
         {
-            return _curScale;
+            return m_db_particle.curScale;
         }
     }
 
@@ -135,42 +49,13 @@ public class UIExParticleSystem : MonoBehaviour {
     {
         get
         {
-            return _maxTime + 0.01f;
+            return m_db_particle.maxTime;
         }
     }
 
     public void SetAlpha(float alpha)
     {
-        int lensRender = listAllRenders.Count;
-        if (lensRender <= 0 || alpha < 0)
-        {
-            return;
-        }
-        Renderer curRender;
-        Material mat;
-        Color col;
-        alpha = alpha > 1 ? alpha / 255f : alpha;
-        for (int i = 0; i < lensRender; i++)
-        {
-            curRender = listAllRenders[i];
-            mat = RenderEx.GetMaterial(curRender);
-            if (mat != null)
-            {
-                if (mat.HasProperty("_Color"))
-                {
-                    col = mat.GetColor("_Color");
-                    col.a = alpha;
-                    mat.SetColor("_Color", col);
-                }
-
-                if (mat.HasProperty("_TintColor"))
-                {
-                    col = mat.GetColor("_TintColor");
-                    col.a = alpha;
-                    mat.SetColor("_TintColor", col);
-                }
-            }
-        }
+        m_db_particle.SetAlpha(alpha);
     }
 
 #if UNITY_EDITOR
@@ -192,7 +77,7 @@ public class UIExParticleSystem : MonoBehaviour {
             setScale(scaleInEditor);
         }
 
-        if (_curState != isPasueInEditor)
+        if (m_db_particle.curState != isPasueInEditor)
         {
             changeState(isPasueInEditor);
         }
