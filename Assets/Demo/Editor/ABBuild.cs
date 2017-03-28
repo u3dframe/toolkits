aaae5c2916fa1e4c2c849c9d81d34a66665eca1f
Canyon <379657505@qq.com>
2017-03-28 18:18:50 +0800

    添加测试文件

---
+++ Assets/Demo/Editor/ABBuild.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ABBuild : Editor {
+	[MenuItem("Window/BuildAB")]
+	static void Build(){		
+		 BuildPipeline.BuildAssetBundles (Application.dataPath + "/Build", 0, EditorUserBuildSettings.activeBuildTarget);
+		// AssetBundleBuild[] bulid = new AssetBundleBuild[5];
+		Core.AssetManager.Test();
+	}
+}
