0dcad4b4e5a1a3ee3beda17bf69a41d4018f07ef
Canyon <379657505@qq.com>
2017-06-05 13:10:07 +0800

    添加一个清除空的无用的资源文件的逻辑

---
+++ Assets/_Frame/_Core/Editor/ClearWindowEditor.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+/// <summary>
+/// 类名 : unity 清空工程无用的区域
+/// 作者 : Canyon
+/// 日期 : 2017-06-05 12:30
+/// 功能 : 
+/// </summary>
+public class ClearWindowEditor : EditorWindow {
+
+	static bool isOpenWindowView = false;
+
+	static protected ClearWindowEditor vwWindow = null;
+
+	// 窗体宽高
+	static public float width = 600;
+	static public float height = 335;
+
+	[MenuItem("Tools/ClearNotUseAsset")]
+	static void AddWindow()
+	{
+		if (isOpenWindowView || vwWindow != null)
+			return;
+
+		try
+		{
+			isOpenWindowView = true;
+			float x = 460;
+			float y = 200;
+			Rect rect = new Rect(x, y, width, height);
+
+			// 大小不能拉伸
+			// vwWindow = GetWindowWithRect<EDW_Skill>(rect, true, "SkillEditor");
+
+			// 窗口，只能单独当成一个窗口,大小可以拉伸
+			//vwWindow = GetWindow<EDW_Skill>(true,"SkillEditor");
+
+			// 这个合并到其他窗口中去,大小可以拉伸
+			vwWindow = GetWindow<ClearWindowEditor>("ClearWindowEditor");
+
+			vwWindow.position = rect;
+		}
+		catch (System.Exception)
+		{
+			OnClearSWindow();
+			throw;
+		}
+	}
+
+	static void OnClearSWindow()
+	{
+		isOpenWindowView = false;
+		vwWindow = null;
+	}
+
+	void OnEnable(){
+		m_Object = new SerializedObject (this);
+		m_Property = m_Object.FindProperty ("_assetList");
+
+		m_PropertyCheck = m_Object.FindProperty ("_checkList");
+	}
+
+	void OnDestroy()
+	{
+		OnClearSWindow();
+	}
+
+	// 在给定检视面板每秒10帧更新
+	void OnInspectorUpdate()
+	{
+		Repaint();
+	}
+
+	//序列化对象
+	SerializedObject m_Object;
+
+	//序列化属性
+	SerializedProperty m_Property;
+
+	//序列化属性
+	SerializedProperty m_PropertyCheck;
+
+	/// <summary>
+	/// 工程用到的
+	/// </summary>
+	[SerializeField]
+	protected List<UnityEngine.Object> _assetList = new List<UnityEngine.Object>();
+
+	/// <summary>
+	/// 需要清除的文件
+	/// </summary>
+	public List<UnityEngine.Object> _checkList = new List<UnityEngine.Object>();
+
+	// 编译 资源
+	EL_AssetClear clearRes = new EL_AssetClear();
+
+	void OnGUI(){
+		EG_GUIHelper.FEG_BeginV ();
+		{
+			EG_GUIHelper.FEG_BeginH ();
+			{
+				GUIStyle style = EditorStyles.label;
+				style.alignment = TextAnchor.MiddleCenter;
+				string txtDecs = "类名 : 资源清除工具\n"
+					+ "作者 : Canyon\n"
+					+ "日期 : 2017-06-05 12:30\n"
+					+ "描述 : 暂无\n";
+				GUILayout.Label(txtDecs, style);
+				style.alignment = TextAnchor.MiddleLeft;
+			}
+			EG_GUIHelper.FEG_EndH ();
+
+			EG_GUIHelper.FG_Space(10);
+
+			clearRes.DrawView(m_Object,m_Property,_assetList,m_PropertyCheck,_checkList);
+		}
+		EG_GUIHelper.FEG_EndV ();
+	}
+}
---
+++ Assets/_Frame/_Core/Editor/Logic/EL_AssetClear.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using Core.Kernel;
+
+/// <summary>
+/// 类名 : 清除 资源
+/// 作者 : Canyon
+/// 日期 : 2017-06-05 12:30
+/// 功能 : 
+/// </summary>
+public class EL_AssetClear{
+	List<UnityEngine.Object> m_listOrg = null;
+
+	List<UnityEngine.Object> m_listCheck = null;
+
+	public void DrawView(SerializedObject obj,SerializedProperty field,List<UnityEngine.Object> list,SerializedProperty fieldClear,List<UnityEngine.Object> listCheck){
+		this.m_listOrg = list;
+		this.m_listCheck = listCheck;
+
+		if (obj == null) {
+			return;
+		}
+
+		obj.Update ();
+
+		EG_GUIHelper.FEG_BeginVArea ();
+
+		EG_GUIHelper.FEG_HeadTitMid ("Clear Not Use Assets",Color.cyan);
+		EG_GUIHelper.FG_Space(10);
+
+		//开始检查是否有修改
+		EditorGUI.BeginChangeCheck();
+
+		//显示属性
+		//第二个参数必须为true，否则无法显示子节点即List内容
+		EditorGUILayout.PropertyField(field,new GUIContent("资源文件夹 : "),true);
+		EG_GUIHelper.FG_Space(10);
+
+		EditorGUILayout.PropertyField(fieldClear,new GUIContent("检测清空资源文件夹 : "),true);
+		EG_GUIHelper.FG_Space(10);
+
+		//结束检查是否有修改
+		if (EditorGUI.EndChangeCheck())
+		{
+			//提交修改
+			obj.ApplyModifiedProperties();
+		}
+
+		EG_GUIHelper.FG_Space(10);
+
+		if (GUILayout.Button("DoBuildAssetU5"))
+		{
+			DoMake();
+		}
+		EG_GUIHelper.FEG_EndV ();
+
+		EG_GUIHelper.FG_Space(10);
+	}
+
+	void DoMake(){
+		if (m_listOrg.Count <= 0) {
+			EditorUtility.DisplayDialog ("Tips", "请选择来源文件夹!!!", "Okey");
+			return;
+		}
+
+		if (m_listCheck.Count <= 0) {
+			EditorUtility.DisplayDialog ("Tips", "请选择需要Check的来源文件夹!!!", "Okey");
+			return;
+		}
+
+		EL_Path.Clear ();
+
+		int lens = m_listOrg.Count;
+		string folderPath = "";
+		for (int i = 0; i < lens; i++) {
+			folderPath = CheckOneFolder (m_listOrg[i]);
+			if (string.IsNullOrEmpty (folderPath))
+				continue;
+			EL_Path.Append(folderPath);
+		}
+
+		// 清空没用的abname
+		AssetDatabase.RemoveUnusedAssetBundleNames ();
+
+	}
+
+	/// <summary>
+	/// 检测获得元素的路径
+	/// </summary>
+	string CheckOneFolder(UnityEngine.Object one){
+		if (one == null)
+			return null;
+
+		System.Type typeFolder = typeof(UnityEditor.DefaultAsset);
+
+		System.Type typeOrg = one.GetType ();
+
+		if (typeOrg != typeFolder) {
+			EditorUtility.DisplayDialog ("Tips", "来源文件不是文件夹!!!", "Okey");
+			return null;
+		}
+
+		return AssetDatabase.GetAssetPath (one);
+	}
+}
